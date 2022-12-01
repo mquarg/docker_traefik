b0c7ac2570db123bee39627e02266a713e08228d
Mirko Quarg <mquarg@gmx.de>
2022-12-01 18:50:51 +0000

    logging improved

--- bin/startTraefik.sh
+++ bin/startTraefik.sh
@@ -2,9 +2,14 @@
 
 CUR_DATE=`date '+%Y%m%d-%H%M'`
 
-cp /var/log/traefik.log /var/log/traefik.log.${CUR_DATE}
-gzip /var/log/traefik.log.${CUR_DATE}
-> /var/log/traefik.log
+LOGFILE=/var/log/traefik.log
+
+if [ -f ${LOGFILE} ]; then
+	echo "LOGFILE: ${LOGFILE}"
+	mv ${LOGFILE} ${LOGFILE}.${CUR_DATE}
+	gzip ${LOGFILE}.${CUR_DATE}
+fi
+touch ${LOGFILE}
 
 INST_DIR=/opt/traefik
 BIN_DIR=${INST_DIR}/bin
@@ -18,12 +23,12 @@ if [ $CNT -eq 1 ]; then
 fi
 
 echo "start Traefik"
-docker run --name traefik -d --restart always -p 8080:8080 -p 80:80 -p 443:443 \
--v /var/log/traefik.log:/var/log/traefik.log \
+docker run --name traefik -d --restart always -p 8080:8080 -p 80:80 -p 443:443 -p 7999:7999 \
+-v ${LOGFILE}:${LOGFILE} \
 -v ${ETC_DIR}/traefik.yml:/etc/traefik/traefik.yml \
 -v ${ETC_DIR}/fileconfig:/etc/traefik/fileconfig \
 -v ${ETC_DIR}/certs:/etc/traefik/certs \
 -v /var/run/docker.sock:/var/run/docker.sock \
 traefik:v2.8.3
 
-tail -f /var/log/traefik.log
+tail -f ${LOGFILE}
